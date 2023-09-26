5683fc5f603050c5d71e5fcd186c797453cee2d5
mak7on22 <mur-mur-0998@mail.ru>
2023-09-26 15:20:00 +0600

    added unums property

---
+++ Delux/Enums/SortState.cs
@@ -0,0 +1,18 @@
+﻿namespace Delux.Enums
+{
+    public enum SortState
+    {
+        NameAsc,
+        NameDesc,
+        PriceAsc,
+        PriceDesc,
+        DateCreateAsc,
+        DateCreateDesc,
+        DateReplAsc,
+        DateReplDesc,
+        CategoryAsc,
+        CategoryDesc,
+        BrandAsc,
+        BrandDesc,
+    }
+}
